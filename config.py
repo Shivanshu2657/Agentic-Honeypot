55594ff6a3ffafb4d6bb44be7905ae4aacd18ab9
Shivanshu2657 <shivanshuchaubey0106@gmail.com>
2026-01-30 10:03:37 +0000

    Fixing SCAM_KEYWORDS

--- config.py
+++ config.py
@@ -1,8 +1,23 @@
 import os
 
+# API security
 API_KEY = os.getenv("API_KEY", "shivanshu-honeypot-key")
 
+# GUVI callback
 GUVI_CALLBACK_URL = os.getenv(
     "GUVI_CALLBACK_URL",
     "https://hackathon.guvi.in/api/updateHoneyPotFinalResult"
 )
+
+# Keywords for intelligence extraction (REQUIRED)
+SCAM_KEYWORDS = [
+    "blocked",
+    "verify",
+    "urgent",
+    "upi",
+    "otp",
+    "click",
+    "link",
+    "suspend",
+    "account"
+]
